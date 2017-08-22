005c6aad1b798fe0103c71c9fd6eeec51abca789
Abdelrahman Omran <me@omranic.com>
2017-08-22 19:39:37 +0200

    Update models docblock

--- src/Models/BookingAvailability.php
+++ src/Models/BookingAvailability.php
@@ -9,6 +9,31 @@ use Illuminate\Database\Eloquent\Model;
 use Rinvex\Cacheable\CacheableEloquent;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+/**
+ * Rinvex\Bookable\Models\BookingAvailability.
+ *
+ * @property int                                                $id
+ * @property int                                                $bookable_id
+ * @property string                                             $bookable_type
+ * @property string                                             $day
+ * @property \Carbon\Carbon                                     $starts_at
+ * @property \Carbon\Carbon                                     $ends_at
+ * @property float                                              $price
+ * @property \Carbon\Carbon                                     $created_at
+ * @property \Carbon\Carbon                                     $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $bookable
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingAvailability whereBookableId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingAvailability whereBookableType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingAvailability whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingAvailability whereDay($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingAvailability whereEndsAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingAvailability whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingAvailability wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingAvailability whereStartsAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingAvailability whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class BookingAvailability extends Model
 {
     use ValidatingTrait;
--- src/Models/BookingRate.php
+++ src/Models/BookingRate.php
@@ -9,6 +9,29 @@ use Illuminate\Database\Eloquent\Model;
 use Rinvex\Cacheable\CacheableEloquent;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+/**
+ * Rinvex\Bookable\Models\BookingRate
+ *
+ * @property int                                                $id
+ * @property int                                                $bookable_id
+ * @property string                                             $bookable_type
+ * @property int                                                $percentage
+ * @property string                                             $operator
+ * @property int                                                $amount
+ * @property \Carbon\Carbon                                     $created_at
+ * @property \Carbon\Carbon                                     $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $bookable
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingRate whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingRate whereBookableId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingRate whereBookableType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingRate whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingRate whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingRate whereOperator($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingRate wherePercentage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Rinvex\Bookable\Models\BookingRate whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class BookingRate extends Model
 {
     use ValidatingTrait;
