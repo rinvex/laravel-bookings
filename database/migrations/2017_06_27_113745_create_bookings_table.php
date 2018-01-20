3abfdb537d60e5e62ead0decb40286511b025715
Abdelrahman Omran <me@omranic.com>
2018-01-20 13:13:09 +0200

    Typehint method returns

--- database/migrations/2017_06_27_113745_create_bookings_table.php
+++ database/migrations/2017_06_27_113745_create_bookings_table.php
@@ -46,7 +46,7 @@ class CreateBookingsTable extends Migration
      *
      * @return string
      */
-    protected function jsonable()
+    protected function jsonable(): string
     {
         return DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME) === 'mysql'
                && version_compare(DB::connection()->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION), '5.7.8', 'ge')
