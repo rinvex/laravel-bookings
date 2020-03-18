85f2106e348a95e92a46e4b4f3e89f17558d0de7
Abdelrahman Omran <me@omranic.com>
2020-03-18 15:17:08 +0400

    Fix migrations path condition

--- src/Console/Commands/RollbackCommand.php
+++ src/Console/Commands/RollbackCommand.php
@@ -31,7 +31,11 @@ class RollbackCommand extends Command
     {
         $this->alert($this->description);
 
-        if (file_exists($path = 'database/migrations/rinvex/laravel-bookings')) {
+        $path = config('rinvex.bookings.autoload_migrations') ?
+            'vendor/rinvex/laravel-bookings/database/migrations' :
+            'database/migrations/rinvex/laravel-bookings';
+
+        if (file_exists($path)) {
             $this->call('migrate:reset', [
                 '--path' => $path,
                 '--force' => $this->option('force'),
